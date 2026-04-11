8c1eef3c764d2018aca665d20528362143be763b
ManishaChaurasiya <manishachaurasiya694@gmail.com>
2026-04-11 23:49:33 +0530

    fix: vercel routing final

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://excel-analytics-platform-qitq.onrender.com",
+  baseURL: "https://excel-analytics-platform-qitq.onrender.com/api",
 });
 
 api.interceptors.request.use(
