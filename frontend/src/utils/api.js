56a7a8fb008687398ab0949250cfa87c018238f6
ManishaChaurasiya <manishachaurasiya694@gmail.com>
2026-04-11 19:21:00 +0530

    fix: login api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://excel-analytics-platform-qitq.onrender.com/api",
+  baseURL: "https://excel-analytics-platform-qitq.onrender.com",
 });
 
 api.interceptors.request.use(
